5ae925723439e438686baae3a8e9b393d30b5a34
Eric Nelson <e82.eric@gmail.com>
2022-12-19 20:36:25 -0800

    Updated SampleConfig

--- SampleConfig.c
+++ SampleConfig.c
@@ -1,7 +1,7 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "SimpleWindowManager.h"
+#include "..\\SimpleWindowManager2\\SimpleWindowManager.h"
 
 long barHeight = 29;
 long gapWidth = 13;
@@ -40,9 +40,31 @@ BOOL chrome_workspace_filter(Client *client)
 
 BOOL terminal_workspace_filter(Client *client)
 {
+    if(wcsstr(client->data->title, L"SimpleMenuTitle"))
+    {
+        return FALSE;
+    }
     if(wcsstr(client->data->processImageName, L"WindowsTerminal.exe"))
     {
+        TCHAR *cmdLine = client_get_command_line(client);
+        if(wcsstr(cmdLine, L"SimpleMenuTitle"))
+        {
+            return FALSE;
+        }
         return TRUE;
+    }
+    return FALSE;
+}
+
+BOOL terminal_with_uniqueStr_filter(ScratchWindow *self, Client *client)
+{
+    if(wcsstr(client->data->processImageName, L"WindowsTerminal.exe"))
+    {
+        TCHAR *cmdLine = client_get_command_line(client);
+        if(wcsstr(cmdLine, self->uniqueStr))
+        {
+            return TRUE;
+        }
     }
     return FALSE;
 }
@@ -88,11 +110,31 @@ BOOL null_filter(Client *client)
     return FALSE;
 }
 
+BOOL virtualboxmanager_workspace_filter(Client *client)
+{
+    if(wcsstr(client->data->title, L"Oracle VM VirtualBox Manager"))
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+BOOL windowsvm_workspace_filter(Client *client)
+{
+    if(wcsstr(client->data->title, L"NewWindows10"))
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+
 Workspace** create_workspaces(int* outSize)
 {
     WCHAR chromeTag = { 0xfa9e };
     WCHAR terminalTag = { 0xf120 };
     WCHAR riderTag = { 0xf668 };
+    WCHAR archTag = { 0xf303 };
+    WCHAR windows10Tag = { 0xe70f };
     WCHAR teamsTag = { 0xf865 };
 
     int* numberOfWorkspaces = malloc(sizeof(int));
@@ -113,15 +155,38 @@ Workspace** create_workspaces(int* outSize)
     return workspaces;
 }
 
-TCHAR *cmdScratchCmd = L"cmd /c powershell -NoLogo";
-TCHAR *launcherCommand = L"cmd /c for /f \"delims=\" %i in ('fd -t f -g \"*{.lnk,.exe}\" \"%USERPROFILE%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\" \"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\" \"%USERPROFILE%\\AppData\\Local\\Microsoft\\WindowsApps\" \"%USERPROFILE%\\Utilites\" ^| fzf --bind=\"ctrl-c:execute(echo {} | clip)\"') do start \" \" \"%i\"";
-TCHAR *allFilesCommand = L"cmd /c set /p \"pth=Enter Path: \" && for /f \"delims=\" %i in ('fd . -t f %^pth% ^| fzf --bind=\"ctrl-c:execute(echo {} | clip)\"') do start \" \" \"%i\"";
+void search_all_drives_command_run(ScratchWindow *self, Monitor *monitor, int scratchWindowsScreenPadding)
+{
+    CHAR cmdBuff[4096];
+    sprintf_s(
+            cmdBuff,
+            4096,
+            "cmd /c powershell -c \
+            \"$drive = (Get-PSDrive -PSProvider FileSystem).Root \
+            | bin\\RunProcess.exe --title ScratchMenu%ls -l %d -t %d -w %d -h %d; \
+            if($drive -eq $null) { exit }; \
+            bin\\RunProcess.exe --namedCommand \"\"\"ld:fd . $($drive)\\\\\"\"\" \
+            --loadCommand ld\" --title ScratchMenu%ls\" -l %d -t %d -w %d -h %d",
+            self->uniqueStr,
+            monitor->xOffset + scratchWindowsScreenPadding,
+            scratchWindowsScreenPadding,
+            monitor->w - (scratchWindowsScreenPadding * 2),
+            monitor->h - (scratchWindowsScreenPadding * 2),
+            self->uniqueStr,
+            monitor->xOffset + scratchWindowsScreenPadding,
+            scratchWindowsScreenPadding,
+            monitor->w - (scratchWindowsScreenPadding * 2),
+            monitor->h - (scratchWindowsScreenPadding * 2));
+
+    process_with_stdout_start(cmdBuff, self->stdOutCallback);
+}
+
 TCHAR *terminalAppPath = L"wt.exe";
-TCHAR *processListCommand1 = L"cmd /c powershell -NoLogo -NoProfile -c . .\\processlist.ps1;Run";
 
 void create_keybindings(Workspace** workspaces)
 {
     keybinding_create_no_args(LAlt, VK_J, select_next_window);
+    keybinding_create_no_args(LAlt, VK_K, select_previous_window);
     keybinding_create_no_args(LAlt, VK_OEM_COMMA, monitor_select_next);
     keybinding_create_no_args(LAlt, VK_N, arrange_clients_in_selected_workspace);
     keybinding_create_no_args(LAlt, VK_L, workspace_increase_master_width_selected_monitor);
@@ -140,6 +205,7 @@ void create_keybindings(Workspace** workspaces)
     keybinding_create_workspace_arg(LAlt, VK_9, swap_selected_monitor_to, workspaces[8]);
 
     keybinding_create_no_args(LShift | LAlt, VK_J, move_focused_client_next);
+    keybinding_create_no_args(LShift | LAlt, VK_K, move_focused_client_previous);
 
     keybinding_create_workspace_arg(LShift | LAlt, VK_1, move_focused_window_to_workspace, workspaces[0]);
     keybinding_create_workspace_arg(LShift | LAlt, VK_2, move_focused_window_to_workspace, workspaces[1]);
@@ -165,17 +231,94 @@ void create_keybindings(Workspace** workspaces)
     keybinding_create_no_args(LAlt, VK_R, redraw_focused_window);
 
     keybinding_create_cmd_args(LWin, VK_T, start_app, terminalAppPath);
-
-    keybinding_create_cmd_args(LAlt, VK_S, start_scratch_not_elevated, allFilesCommand);
-    keybinding_create_cmd_args(LShift | LAlt, VK_S, start_launcher, allFilesCommand);
-    keybinding_create_cmd_args(LWin, VK_0, start_scratch_not_elevated, cmdScratchCmd);
-    keybinding_create_cmd_args(LShift | LWin, VK_0, start_launcher, cmdScratchCmd);
-    keybinding_create_cmd_args(LShift | LAlt, VK_P, start_launcher, launcherCommand);
-    keybinding_create_cmd_args(LAlt, VK_P, start_scratch_not_elevated, launcherCommand);
-    keybinding_create_cmd_args(LWin, VK_9, start_launcher, processListCommand1);
-    keybinding_create_no_args(LAlt, VK_SPACE, open_windows_scratch_start);
-
     keybinding_create_no_args(LAlt, VK_F9, quit);
+
+    scratch_terminal_register(
+            "cmd /c vifm",
+            LAlt,
+            VK_Q,
+            L"4d237b71-4df7-4d2e-a60f-f9d7f69e80a7",
+            terminal_with_uniqueStr_filter);
+
+    scratch_terminal_register(
+            "cmd /c powershell -nologo",
+            LAlt | LShift,
+            VK_W,
+            L"ea3f98ca-973f-4154-9a7b-dc7a9377b768",
+            terminal_with_uniqueStr_filter);
+
+    scratch_terminal_register(
+            "cmd /c nvim -c \"terminal powershell -nologo\"",
+            LAlt,
+            VK_W,
+            L"643763f5-f5cd-416e-a5c9-bef1f516863c",
+            terminal_with_uniqueStr_filter);
+
+    scratch_menu_register(
+            "--namedCommand \"ld:bin\\ListWindows.exe\" --loadCommand \"ld\"",
+            open_windows_scratch_exit_callback,
+            LAlt,
+            VK_SPACE,
+            L"127ac0a0-9f30-47ef-b225-1dda332beb19");
+
+    scratch_menu_register2(
+            NULL,
+            open_program_scratch_callback_not_elevated,
+            LAlt,
+            VK_E,
+            L"8f631b05-9d5d-4c19-b3b7-f285835785ea",
+            search_all_drives_command_run);
+
+    scratch_menu_register(
+            "--namedCommand \"ld:fd -t f -g \"\"*{.lnk,.exe}\"\" \
+            \"\"%PROGRAMFILES%\"\" \
+            \"\"%PROGRAMFILES(X86)%\"\" \
+            \"\"%USERPROFILE%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\"\" \
+            \"\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\"\" \
+            \"\"%USERPROFILE%\\AppData\\Local\\Microsoft\\WindowsApps\"\" \
+            \"\"%USERPROFILE%\\Utilites\"\"\" --loadCommand \"ld\"",
+            open_program_scratch_callback,
+            LAlt | LShift,
+            VK_P,
+            L"dd07e19f-5d2f-427e-9c8f-ad548ef0acfb");
+
+    scratch_menu_register(
+            "--namedCommand \"ld:fd -t f -g \"\"*{.lnk,.exe}\"\" \
+            \"\"%USERPROFILE%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\"\" \
+            \"\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\"\" \
+            \"\"%USERPROFILE%\\AppData\\Local\\Microsoft\\WindowsApps\"\" \
+            \"\"%USERPROFILE%\\Utilites\"\"\" --loadCommand \"ld\"",
+            open_program_scratch_callback_not_elevated,
+            LAlt,
+            VK_P,
+            L"8f6579ce-727c-4d5b-9e76-2cdd1db52e52");
+
+    scratch_menu_register(
+            "--hasHeader \
+            --loadCommand \"ld\" --namedCommand \"ld:bin\\ListProcesses.exe\" \
+            --namedCommand \"procKill:cmd /c taskkill /f /pid {}\" --bind \"ctl-k:procKill\" --returnRange 46,8 \
+            --namedCommand \"windbg:powershell.exe -C '{}' -match '\\d{8}';windbg -p \"\"\"$([int]$Matches[0])\"\"\"\" \
+            --namedCommand \"sortPvtBytes:bin\\ListProcesses.exe --sort privateBytes\" \
+            --namedCommand \"sortWrkSet:bin\\ListProcesses.exe --sort workingSet\" \
+            --namedCommand \"sortCpu:bin\\ListProcesses.exe --sort cpu\" \
+            --bindLoadCommand \"ctl-1:sortPvtBytes\" \
+            --bindLoadCommand \"ctl-2:sortWrkSet\" \
+            --bindLoadCommand \"ctl-3:sortCpu\" \
+            --bind \"ctl-d:windbg\"",
+            open_process_list_scratch_callback,
+            LWin,
+            VK_9,
+            L"1a19cef1-1d1c-47b5-84c3-e5e2d1ffe141");
+}
+
+BOOL should_float_be_focused(Client *client)
+{
+    if(wcsstr(L"Microsoft Teams Notification", client->data->title))
+    {
+        return FALSE;
+    }
+
+    return TRUE;
 }
 
 BOOL should_always_exclude(Client* client)
@@ -202,6 +345,10 @@ BOOL is_float_window(Client *client, LONG_PTR styles, LONG_PTR exStyles)
 
     if(styles & WS_POPUP)
     {
+        if(wcsstr(client->data->className, L"Qt5152QWindowIcon"))
+        {
+            return FALSE;
+        }
         if(wcsstr(client->data->className, L"Qt5QWindowIcon"))
         {
             return FALSE;
